fc83e289282af58b84893023f43561c2cd44b7fa
emilkjuka <emilkjuka@gmail.com>
2022-10-22 16:18:39 +0200

    added temporary logo

--- to-do/src/components/IconSidebar.svelte
+++ to-do/src/components/IconSidebar.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
     export let imageSource;
     export let addProps;
+
+    function onClickHandler(){
+        console.log("sidebar icon clicked!")
+    }
 </script>
 
-<div class="flex justify-center align-middle rounded-md {addProps}">
-    <img alt="img mis" src={imageSource}/>
+<div class="flex justify-center align-middle rounded-md h-[50px] w-[50px]  hover:bg-slate-400 {addProps}" on:click={onClickHandler} on:keypress>
+    <img alt="img" class=" m-2 h-[32px] w-[32px]" src={imageSource}/>
 </div>
-
-<style>
-
-</style>
--- to-do/src/components/Sidebar.svelte
+++ to-do/src/components/Sidebar.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
-    import IconSidebar from "./IconSidebar.svelte";
+    import IconSidebar from "./IconSidebar.svelte"
 </script>
 
-<div class="w-[4%] h-full bg-purple-400 pt-8 min-w-[50px]">
-    <IconSidebar addProps={""} imageSource={"../../assets/folder.png"} />
-    <div class="p-2 mt-2">
-        <IconSidebar addProps={""} imageSource={"../../assets/folder.png"} />
-        <IconSidebar addProps={""} imageSource={"../../assets/folder.png"} />
-        <IconSidebar addProps={""} imageSource={"../../assets/folder.png"} />
-        <IconSidebar addProps={""} imageSource={"../../assets/folder.png"} />
+<div class="w-[4%] h-full bg-gray-800 pt-8 min-w-[50px] flex flex-col items-center">
+    <div class="text-white text-lg font-bold mb-4"> . . . </div>
+    <img class=" h-[40px] w-[40px]" src="../../assets/logo.svg" alt="logo"/>
+    <div class="p-2 mt-2 ">
+        <IconSidebar addProps={"mt-4"} imageSource={"../../assets/folder.png"} />
+        <IconSidebar addProps={"mt-4"} imageSource={"../../assets/folder.png"} />
+        <IconSidebar addProps={"mt-4"} imageSource={"../../assets/folder.png"} />
+        <IconSidebar addProps={"mt-4"} imageSource={"../../assets/folder.png"} />
+        <IconSidebar addProps={"mt-4"} imageSource={"../../assets/folder.png"} />
+        <IconSidebar addProps={"mt-4"} imageSource={"../../assets/folder.png"} />
+        <IconSidebar addProps={"mt-4"} imageSource={"../../assets/folder.png"} />
+        <IconSidebar addProps={"mt-4"} imageSource={"../../assets/folder.png"} />
     </div>
 </div>
+
+<style>
+
+</style>

61368c78366ab1e84d0e0529c3d03b0c17cedddb
emilkjuka <emilkjuka@gmail.com>
2022-10-22 16:30:37 +0200

    Started working on projects pane
    [work-in-progress]

--- to-do/src/components/ProjectsPane.svelte
+++ to-do/src/components/ProjectsPane.svelte
@@ -3,7 +3,11 @@
     
 </script>
 
-<div class="h-full w-[13%] min-w-[170px] bg-blue-200">
+<div class="h-full w-[13%] min-w-[170px] bg-white flex flex-col justify-start">
+    <div class="flex justify-between items-center p-4">
+        <div class="text-[2em] font-bold">Projects</div>
+        <img class="w-[32px] h-[32px] bg-gray-200 rounded-[90px]" src="../../assets/plus.png" alt="plus" /> 
+    </div>
 
 </div>
 
--- to-do/src/components/Sidebar.svelte
+++ to-do/src/components/Sidebar.svelte
@@ -4,7 +4,7 @@
 
 <div class="w-[4%] h-full bg-gray-800 pt-8 min-w-[50px] flex flex-col items-center">
     <div class="text-white text-lg font-bold mb-4"> . . . </div>
-    <img class=" h-[40px] w-[40px]" src="../../assets/logo.svg" alt="logo"/>
+    <img class=" h-[40px] w-[40px] mt-4" src="../../assets/logo.svg" alt="logo"/>
     <div class="p-2 mt-2 ">
         <IconSidebar addProps={"mt-4"} imageSource={"../../assets/folder.png"} />
         <IconSidebar addProps={"mt-4"} imageSource={"../../assets/folder.png"} />

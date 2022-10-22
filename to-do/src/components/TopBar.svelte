cc7892bc1185b5f2029e284ad9dd765062e80ff1
emilkjuka <emilkjuka@gmail.com>
2022-10-22 15:24:24 +0200

    TopBar component complete
    [no-issue] top bar completet with clickable items
    icons to be changed

--- to-do/src/components/TopBar.svelte
+++ to-do/src/components/TopBar.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
+    import TopBarIcon from "./TopBarIcon.svelte";
+    let name = "Emil"
+    let now = new Date().toLocaleDateString()
+
 
 </script>
 
 
-<div class="w-[100%] h-[6%] bg-yellow-400">
-
+<div class="w-[100%] h-[6%] flex justify-between items-center bg-white min-h-[50px]">
+    <div class=" font-bold ml-4 p-2 text-[1.5rem]">Welcome back, {name}! 👋</div>
+    <div class="flex justify-evenly p-2 ml-2 mr-2 text-[1.5rem] w-[500px] items-center">
+        <TopBarIcon imgSource={"../../assets/folder.png"} extraAtr={""}></TopBarIcon>
+        <TopBarIcon imgSource={"../../assets/folder.png"} extraAtr={""}></TopBarIcon>
+        <TopBarIcon imgSource={"../../assets/folder.png"} extraAtr={""}></TopBarIcon>
+        <div class="ml-2 mr-2 text-gray-400">{now}</div>
+        <TopBarIcon imgSource={"../../assets/pp.png"} extraAtr={"rounded-xl"}></TopBarIcon>
+    </div>
 </div>
---
+++ to-do/src/components/TopBarIcon.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+    export let imgSource;
+    export let extraAtr;
+
+    function onClickHandler(){
+        console.log("clicked!")
+    }
+</script>
+
+<div class="hover:bg-slate-200 rounded-xl p-1 w-[40px] h-[40px]" on:click={onClickHandler} on:keypress>
+    <img class="h-[32px] w-[32px] {extraAtr}" alt="img" src={imgSource}/>
+</div>

45aea876091ebb498209d63ba6d6d3eba04640ad
emilkjuka <emilkjuka@gmail.com>
2022-10-23 23:07:04 +0200

    more changes

--- to-do/src/components/TaskItem.svelte
+++ to-do/src/components/TaskItem.svelte
@@ -3,6 +3,13 @@
 </script>
 
 
-<div class="bg-white w-[95%] h-[20%]">
-
+<div class="bg-white w-[95%] h-[20%] mt-2 rounded-xl border-gray-400 border-1">
+    <div class="flex flex-col justify-start w-[95%] h-[90%] p-2">
+        <div class="font-bold text-[1.5rem]">
+            Item Title
+        </div>
+        <div class="font-bold text-[1rem]">
+            Item subtitle
+        </div>
+    </div>
 </div>
--- to-do/src/components/TaskWindow.svelte
+++ to-do/src/components/TaskWindow.svelte
@@ -3,15 +3,19 @@
 
     let index = 1;
     let items = [index];
+
+    function createNewTask(){
+        items=[index, ...items]
+    }
 </script>
 
 <div
     class="h-[95%] w-[428px] rounded-[30px] border-dashed border-4 border-black flex flex-col justify-start items-center m-6 pt-6"
 >
-    <div class="m-1 flex justify-center items-between">
-        <div class="bg-white ml-2 mr-2 pl-2 pr-2">Title</div>
-        <div class="bg-white  ml-2 mr-2 pl-2 pr-2">
-            <button>New Task</button>
+    <div class="mt-2 w-[95%] flex justify-between ">
+        <div class="bg-gray-200 mr-2 pl-2 pr-2 rounded-lg">To-Do</div>
+        <div class="bg-gray-200  ml-2  pl-2 pr-2 rounded-lg">
+            <button on:click={createNewTask}>New Task</button>
         </div>
     </div>
     {#each items as item}

636c8c21e8f5fc4ea8b038b2b309461ae3abb1fe
emilkjuka <emilkjuka@gmail.com>
2022-10-24 19:50:57 +0200

    Scrolling functionality when items overflow mainbody
    [no-issue] when more views are added that the width of the main body then scrolling is enabled

--- to-do/src/components/MainBody.svelte
+++ to-do/src/components/MainBody.svelte
@@ -19,16 +19,18 @@
     }
 </script>
 
-<div class="h-[5%] min-h-[30px] flex justify-between items-center">
-    <button class="rounded-xl bg-white w-[96px] m-4" on:click={createNewTaskWindow}>
-        + Add view
-    </button>
-    <button class="rounded-xl bg-white w-[96px] m-4" on:click={deleteTaskWindow}>
-        - Delete view
-    </button>
-</div>
-<div class="h-[94%] w-full flex items-center justify-start bg-slate-300 pl-8">
-    {#each windows as window}
+<div class="h-full w-full overflow-hidden">
+    <div class="h-[5%] min-h-[30px] flex justify-between items-center">
+        <button class="rounded-xl bg-white w-[96px] m-4" on:click={createNewTaskWindow}>
+            + Add view
+        </button>
+        <button class="rounded-xl bg-white w-[96px] m-4" on:click={deleteTaskWindow}>
+            - Delete view
+        </button>
+    </div>
+    <div class="h-[95%] max-w-inherit flex items-center justify-start bg-slate-300 pl-8 overflow-x-auto overflow-y-hidden">
+        {#each windows as window}
         <TaskWindow></TaskWindow>
-    {/each}
+        {/each}
+    </div>
 </div>
--- to-do/src/components/TaskWindow.svelte
+++ to-do/src/components/TaskWindow.svelte
@@ -4,15 +4,13 @@
     let index = 1;
     let items = [index];
 
-    function createNewTask(){
-        items=[index, ...items]
+    function createNewTask() {
+        items = [index, ...items];
     }
 </script>
 
-<div
-    class="h-[95%] w-[428px] rounded-[30px] border-dashed border-4 border-black flex flex-col justify-start items-center m-6 pt-6"
->
-    <div class="mt-2 w-[95%] flex justify-between ">
+<div class="h-[95%] w-[428px] min-w-[420px] rounded-[30px] border-dashed border-4 border-black flex flex-col justify-start items-center m-6 pt-6">
+    <div class="mt-2 w-[95%]  flex justify-between ">
         <div class="bg-gray-200 mr-2 pl-2 pr-2 rounded-lg">To-Do</div>
         <div class="bg-gray-200  ml-2  pl-2 pr-2 rounded-lg">
             <button on:click={createNewTask}>New Task</button>

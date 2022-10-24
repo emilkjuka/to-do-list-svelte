490c3ebbb0f64fde062e0160484010a8b3fcb678
emilkjuka <emilkjuka@gmail.com>
2022-10-24 22:46:38 +0200

    Update ProjectContainer.svelte
    Final touches to version 1.0 complete

--- to-do/src/components/ProjectContainer.svelte
+++ to-do/src/components/ProjectContainer.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-    let contentProjects = ">"
-    let opValueProjects = 0
-    let heightValueProjects = "0"
+   let contentProjects = "+"
+    let opValueProjects = "0"
+    let heightValueProjects = "[0px]"
     
-    let contentTasks = ">"
-    let opValueTasks = 0
-    let heightValueTasks = "0"
+    let contentTasks = "+"
+    let opValueTasks = "0"
+    let heightValueTasks = "[0px]"
 
     function onClickHideTasks(){
-        contentTasks == ">" ? contentTasks="˅" : contentTasks=">"
-        opValueTasks == 0 ? opValueTasks=100 : opValueTasks = 0
-        heightValueTasks == "0" ? heightValueTasks = "auto" : heightValueTasks="0"
+        contentTasks == "-" ? contentTasks="+" : contentTasks="-"
+        opValueTasks == "0" ? opValueTasks= "" : opValueTasks = "0"
+        heightValueTasks == "[0px]" ? heightValueTasks = "[auto]" : heightValueTasks="[0px]"
     }
     
     function onClickHideProject(){
-        contentProjects == ">" ? contentProjects="˅" : contentProjects=">"
-        opValueProjects == 0 ? opValueProjects=100 : opValueProjects = 0
-        heightValueProjects == "0" ? heightValueProjects = "auto" : heightValueProjects="0"
+        contentProjects == "-" ? contentProjects="+" : contentProjects="-"
+        opValueProjects == "0" ? opValueProjects="" : opValueProjects = "0"
+        heightValueProjects == "[0px]" ? heightValueProjects = "[auto]" : heightValueProjects="[0px]"
     }
 
 </script>
@@ -26,24 +26,20 @@
         <div>Projects</div>
         <button on:click={onClickHideProject}>{contentProjects}</button>
     </div>
-    <div><vr></div>
     <div class="ml-2 pl-4 pt-2 pb-2 font-bold border-l-2 border-gray-200 opacity-{opValueProjects} h-{heightValueProjects}">
         <div class="mt-4">All Projects(3)</div>
         <div class="mt-4">Main Project</div>
         <div class="mt-4">Second Project</div>
         <div class="mt-4">Third Project</div>
     </div>
-    <div class="flex justify-between text-[1.5rem] font-bold">
+    <div class="flex justify-between text-[1.5rem] font-bold mt-4">
         <div>Tasks</div>
         <button on:click={onClickHideTasks}>{contentTasks}</button>
     </div>
-    <div><vr></div>
     <div class="ml-2 pl-4 pt-2 font-bold border-l-2 border-gray-200 opacity-{opValueTasks} h-{heightValueTasks}">
-        <div class="mt-4">All Projects(3)</div>
-        <div class="mt-4">Main Project</div>
-        <div class="mt-4">Second Project</div>
-        <div class="mt-4">Third Project</div>
+        <div class="mt-4">All Tasks(3)</div>
+        <div class="mt-4">Main task</div>
+        <div class="mt-4">Second task</div>
+        <div class="mt-4">Third task</div>
     </div>
-    
-
 </div>
